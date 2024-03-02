0554084dd8d02b60407402d7e66cb9182edaad03
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-03-02 11:43:56 -0300

    feat: logout feature

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,13 +1,36 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import { useUserContext } from "../../contexts/UserContext";
+import Button from "../../components/Button/Button";
+import userService from "../../services/userService.ts";
+import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Profile = () => {
   const { user } = useUserContext();
+  const navigation = useNavigation();
+
+  const handleLogout = () => {
+    try {
+      userService.logout().then((response) => {
+        console.log(response);
+
+        if (response?.status === 200) {
+          navigation.navigate("login");
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   if (user)
     return (
       <View>
-        {user.name} - {user.email}
+        <Text>
+          {user.name} - {user.email}
+        </Text>
+        <Button text="Sair" onClick={handleLogout} />
       </View>
     );
   else return null;
